04249ea0f78492184bd20c468116d3674eb01a27
Naomi Guyer <naomi@silverstripe.com>
2014-05-04 21:08:52 +1200

    Initial mobile menu js

--- js/general.js
+++ js/general.js
@@ -6,4 +6,41 @@ jQuery(function($) {
 
 	// Hello there! You can add in any of your site-specific code here.
 
+
+
+});
+
+
+
+jQuery(window).on('load resize orientationchange', function() {
+	"use strict";
+
+	var width = $(window).width() / parseFloat($("body").css("font-size")),
+		desktopwidth = 48;
+
+	//Add again if desktop
+	$('#navbar').removeClass('pure-menu-open');
+
+	if (width <= desktopwidth && !$('html').hasClass('oldie')) {
+		$('body').removeClass('desktop').addClass('mobile');
+	} 
+	else {
+		$('body').removeClass('mobile').addClass('desktop');
+	}
+
+	if (width >= desktopwidth) {
+		$('#navbar').addClass('pure-menu-open');
+		console.log('true');
+	} else {
+
+		
+
+		$('.menu-trigger').on('click', function() {
+			if($('#navbar').hasClass('pure-menu-open')){
+				$('#navbar').removeClass('pure-menu-open');
+			} else{
+				$('#navbar').addClass('pure-menu-open');
+			}
+		});
+	}
 });
--- templates/Includes/Navbar.ss
+++ templates/Includes/Navbar.ss
@@ -1,6 +1,6 @@
 
 <div class="pure-g">
-	<nav id="navbar" class="menu-default pure-menu pure-menu-open pure-menu-horizontal pure-u-1" role="navigation">
+	<nav id="navbar" class="menu-default pure-menu pure-menu-horizontal pure-u-1" role="navigation">
 
 		<h2 class="accessibility-nonvisual-indicator">Main navigation</h2>
 		<ul class="">
